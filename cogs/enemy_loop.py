59443b91ecd870d34a6581253c4fd7bc432bed48
scoot <bitsgod1337@gmail.com>
2021-08-11 14:12:54 +0200

    Fixed bot_settings.py and enemy_loop.py

--- cogs/enemy_loop.py
+++ cogs/enemy_loop.py
@@ -4,6 +4,8 @@ from discord.ext import commands
 from discord.ext.commands import Cog
 from cogs.enemy import EnemyModule
 
+from core.bot_settings import ENEMY_LOOP_CHANNEL
+
 class EnemyLoop(Cog):
     def __init__(self, bot) -> None:
         self.bot = bot
@@ -15,7 +17,7 @@ class EnemyLoop(Cog):
 
     async def run_loop(self):
         while True:
-            channel = self.bot.get_channel(874280730010734682)
+            channel = self.bot.get_channel(ENEMY_LOOP_CHANNEL)
             await channel.send(embed=EnemyModule.check_enemy_activity(EnemyModule.get_enemy_list()))
             await asyncio.sleep(60)
 
